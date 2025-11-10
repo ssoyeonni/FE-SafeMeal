651c06598f9fcaa9f269879a3a01927d9c5dadef
thdus <thduswja23@gmail.com>
2025-11-11 01:46:18 +0900

    Feat: 더미데이터 분리

---
+++ src/data/recipesList.js
@@ -0,0 +1,27 @@
+
+export const recipesList = [
+  {
+    id: 1,
+    category: "밥",
+    color: "bg-[#BBE491]/50",
+    name: "두부 곤약 나물 비빔밥",
+    kcal: 225,
+    image: "https://cdn-icons-png.flaticon.com/512/706/706164.png",
+  },
+  {
+    id: 2,
+    category: "국&찌개",
+    color: "bg-[#E491BB]/50",
+    name: "표고버섯 청경채국",
+    kcal: 45,
+    image: "https://cdn-icons-png.flaticon.com/512/706/706164.png",
+  },
+  {
+    id: 3,
+    category: "반찬",
+    color: "bg-[#91BBE4]/50",
+    name: "시금치 우유 소스와 그린매쉬드 포테이토",
+    kcal: 155,
+    image: "https://cdn-icons-png.flaticon.com/512/706/706164.png",
+  },
+];
---
+++ src/data/shortForm.js
@@ -0,0 +1,19 @@
+import bibimbapVideo from "../assets/short-form/bibimbap.mp4";
+import shrimpTofuSteamedEggsVideo from "../assets/short-form/shrimp_tofu_eggs.mp4";
+
+export const shortForm = [
+  {
+    id: 1,
+    src: bibimbapVideo,
+    title: "든든한 한끼! 건강한 버섯 비빔밥 만들기",
+    author: "@요리왕 김셰프",
+    likes: 12000,
+  },
+  {
+    id: 2,
+    src: shrimpTofuSteamedEggsVideo,
+    title: "10분 완성! 간단 새우두부계란찜",
+    author: "@쿡쿡TV",
+    likes: 9800,
+  },
+];
